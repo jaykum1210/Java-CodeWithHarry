dfe6afb208258a89dc79c15fa7510dcc2135ccb6
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-12 01:45:38 +0530

    add associativity file

---
+++ Videos/Associativity.java
@@ -0,0 +1,20 @@
+class Associativity{
+    public static void main(String[] args) {
+        int a = 6*5-34/2;
+        System.out.println(a);
+        int b = 60/5-34*2;
+        System.out.println(b);
+
+        // Quick Quiz
+
+        int x = 6,y = 1;
+        int c = x*y/2;
+        System.out.println(c);
+
+        // Quick Quiz
+        int l = 1,m=4,n=5;
+
+        int k = l*l - 4*n*m/(2*n);
+        System.out.println(k);
+    }
+}
