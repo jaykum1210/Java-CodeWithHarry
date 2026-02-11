9908ee770cf9f02d22272a6dc226dfaa8491b57a
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-12 00:36:41 +0530

    add literal file

---
+++ Videos/Input1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Input1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Taking Integer Input
+        // System.out.println("Enter Numebr 1 : ");
+        // int a = sc.nextInt();
+        // System.out.println("Enter Number 2 : ");
+        // int b = sc.nextInt();
+        // System.out.println("Summ of " + a + " and " + b + " = " + (a+b));
+
+        // // Taking Float Input
+        // System.out.println("Enter Float Value : ");
+        // float c = sc.nextFloat();
+        // System.out.println(c);
+
+        // // Check Input Integer or not
+
+        // System.out.println("Enter Integer : ");
+        // boolean b1 = sc.hasNextInt();
+        // System.out.println(b1);
+
+        // Input String
+
+
+        System.out.println("Enter string 1 : ");
+        // String s1 = sc.next();       //Only 1 Word
+        String s2 = sc.nextLine();   // Whole Line
+        // System.out.println(s1);
+        System.out.println(s2);
+    }
+}
