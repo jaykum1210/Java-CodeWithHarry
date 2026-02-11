05e5715779fe04892ddbf64e9fb2bacaef9b17b5
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-12 00:25:40 +0530

    add literal file

---
+++ Literals.java
@@ -0,0 +1,20 @@
+class Literals{
+    public static void main(String[] args) {
+        byte b = 32;
+        int i = 56;
+        short s = 87;
+        long l = 4654654564564L;
+        char a = 'A';
+        float f = 10.23f;
+        double d = 10.56465;
+        boolean t = true;
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(a);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(t);
+    }
+}
