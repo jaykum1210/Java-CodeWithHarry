5374b9c8b4330b3788d7abea1da330cbd9e9dc56
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-12 23:00:25 +0530

    add String1 file

---
+++ Videos/String1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class String1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = "Jay";
+        System.out.println(a);
+        String b = sc.nextLine();
+        System.out.println(b);
+    }
+}
