4799d59dffbbc6d1e4a894f0bcade323d7049acd
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-13 22:55:01 +0530

    add string method file

---
+++ Practice Ste/Chapter3.java
@@ -0,0 +1,12 @@
+class Chapter3{
+    public static void main(String[] args) {
+        String a = "   Hello Jay!";
+        // Lowercase
+        System.out.println("Lowercase : " + a.toLowerCase());
+        
+        //Replace space with underscore
+        System.out.println("Replaced : " + a.replace(' ','_'));
+
+        // 
+    }    
+}
