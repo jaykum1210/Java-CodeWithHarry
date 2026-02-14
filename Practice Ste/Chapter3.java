332dcb6e9a93ae1bf6c0e25262e2b77757587261
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-14 11:01:25 +0530

    update chapter3 file

--- Practice Ste/Chapter3.java
+++ Practice Ste/Chapter3.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 class Chapter3{
     public static void main(String[] args) {
         String a = "   Hello Jay!";
@@ -7,6 +10,18 @@ class Chapter3{
         //Replace space with underscore
         System.out.println("Replaced : " + a.replace(' ','_'));
 
-        // 
+        // make a sntance
+        Scanner sc = new Scanner(System.in);
+        String b = sc.nextLine();
+        System.out.println("Dear " + b + " , Thanks a lot");
+
+        // Double and Triple
+        String c = "Hello  Hii  ,Jay";
+        System.out.println(c.indexOf("  "));
+        System.out.println(c.indexOf("   "));
+
+        // Escape sequencce
+        String d = "Dear Jay,\nThis Java Course is Nice.\nThanks!";
+        System.out.println();
     }    
 }
