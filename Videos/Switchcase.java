a75e133eb1f95add7e722b414f8a614cb135a6e1
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-14 11:26:34 +0530

    add switch case file

---
+++ Videos/Switchcase.java
@@ -0,0 +1,18 @@
+class Switchcase{
+    public static void main(String[] args) {
+        int option = 1;
+        switch (option) {
+            case 1:
+                System.out.println("option 1");
+                break;
+            case 2:
+                System.out.println("option 2");
+                break;
+            case 3:
+                System.out.println("option 3");
+                break;
+            default:
+                System.out.println("not valid");
+        }
+    }
+}
