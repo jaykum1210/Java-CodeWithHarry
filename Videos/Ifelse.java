76fc175f4ba86bd51fe3d3ee22d9b55e39f4c4b0
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-14 11:06:42 +0530

    add if else file

---
+++ Videos/Ifelse.java
@@ -0,0 +1,11 @@
+class Ifelse{
+    public static void main(String[] args) {
+        int age = 20;
+        if(age>18){
+            System.out.println("Older");
+        }
+        else{
+            System.out.println("Younger");
+        }
+    }
+}
